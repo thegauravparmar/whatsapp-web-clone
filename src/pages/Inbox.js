c9663e0cf8f812ea30b9520321b1663754e5bc9d
H4ckOm <gparmar1522@gmail.com>
2023-01-11 18:47:41 +0530

    feat: Inbox>> Search Section

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -2,12 +2,29 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import UserSection from "./subs/UserSection";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Inbox(props) {
+  const [list, setList] = useState("");
   // Render the component
   return (
     <div className="inbox rel flex col">
       <UserSection />
+      <div className="search flex">
+        <div className="search-section flex align">
+          <button className="fs20">
+            <SearchIcon />
+          </button>
+          <input
+            placeholder="Search or start a new chat"
+            className="fs15 font"
+            // Update list state variable when input value is changed
+            onChange={(event) => {
+              setList(event.target.value);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
