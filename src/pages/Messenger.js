9b4b38e94a26e7cb2fa23269eaddb3517e53ae73
H4ckOm <gparmar1522@gmail.com>
2023-01-11 18:33:04 +0530

    feat: Inbox and chatbox

--- src/pages/Messenger.js
+++ src/pages/Messenger.js
@@ -1,6 +1,16 @@
 import React from "react";
-
+import ChatBox from "./ChatBox";
+import Inbox from "./Inbox";
+// Messenger component renders the ChatBox and Inbox components to the page
 function Messenger(props) {
-  return <>Hi its messenser</>;
+  return (
+    <>
+      <div className="green-head"></div>
+      <div className="messenger fixed flex">
+        <Inbox />
+        <ChatBox />
+      </div>
+    </>
+  );
 }
 export default Messenger;
